d5c075e85a7ccbca9bf48af5b2c73cd8606f4048
mugala <mugalalevi21@gmail.com>
2018-05-04 11:48:56 +0300

    View function This commit will add the view function for convert days and past pics as well as add the welcome.html page and register the pics app in settings.py

--- my_gallery/pics/urls.py
+++ my_gallery/pics/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     url('^$', views.welcome, name = 'welcome'),  #url to direct to the home page
-    url('^today/$', views.todays_pics, name = 'todaysPics'),  #url to direct to pictures posted today
+    url('^today/$', views.todays_pics, name = 'todaysPics'),  #url to direct to pictures posted today.
+    url(r'^archives/(\d{4}-\d{2}-\d{2})/$', views.past_pics, name = 'pastPics'),  #url to direct to pictures posted in the past.
 ]

3d2c409cbb7d39f14476aa7d43abb48e3c6b0e94
mugala <mugalalevi21@gmail.com>
2018-05-04 15:36:21 +0300

    First Model This commit will create the first model for images with columns for ID name desc location and category

--- my_gallery/pics/models.py
+++ my_gallery/pics/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class image (models.Model):
+    image_name = models.CharField(max_length =30)
+    image_description = models.CharField(max_length =200)
+    image_location = models.CharField(max_length=50)
+    image_category = models.CharField(max_length=40)
+

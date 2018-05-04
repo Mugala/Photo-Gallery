5bc0171df1fc90d2ec2191292cc4c9ed2ba18519
mugala <mugalalevi21@gmail.com>
2018-05-04 15:51:36 +0300

    Category model This commit will create a model for category with columns for ID and name of the category

--- my_gallery/pics/models.py
+++ my_gallery/pics/models.py
@@ -2,9 +2,26 @@ from django.db import models
 
 # Create your models here.
 
-class image (models.Model):
+class Image (models.Model):
     image_name = models.CharField(max_length =30)
     image_description = models.CharField(max_length =200)
     image_location = models.CharField(max_length=50)
     image_category = models.CharField(max_length=40)
+
+    #returns a string representation of the model, useful for when we view return queries.
+
+    def __str__(self):
+        return self.image_name
+    
+    #to specify model-specific options.
+    class Meta:
+        ordering = ['image_name']
+        
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
 
